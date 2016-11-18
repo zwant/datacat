2b512de8cb7ee0d1edb9f323880f791cc9564e14
Svante Paldan <svantepaldan@spotify.com>
2016-11-18 17:59:10 +0100

    Adding an example python script, not working right now

---
+++ example_collectors/python/example.py
@@ -0,0 +1,30 @@
+import time
+import random
+import zmq
+import json
+
+def main():
+    context = zmq.Context()
+    # First, connect our subscriber socket
+    subscriber = context.socket(zmq.SUB)
+    subscriber.connect('tcp://127.0.0.1:5555')
+    subscriber.setsockopt(zmq.SUBSCRIBE, b'')
+
+    # First, connect our publish socket
+    publisher = context.socket(zmq.PUB)
+    publisher.connect('tcp://127.0.0.1:5556')
+
+    while True:
+        time.sleep(2)
+        #subscriber.recv()
+        data = {
+            "name": "My Python Script",
+            "value": random.uniform(0, 100)
+        }
+        print "Sending {}".format(data)
+
+        result = publisher.send(json.dumps(data) + "\n")
+        print result
+
+if __name__ == "__main__":
+    main()
